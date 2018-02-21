a01512f062d9e11ef998b22fe086688f914f263b
HariPriya <Hari Priya@HariPriya-PC>
2018-02-20 23:18:52 -0500

    Adding isHariSleeping method

--- src/main/java/com/testpackage/TestClass.java
+++ src/main/java/com/testpackage/TestClass.java
@@ -1,8 +1,11 @@
 package com.testpackage;
 
 public class TestClass {
-public static void main(String args[]) {
-	System.out.println("Hi");
-	
-}
+	public static void main(String args[]) {
+		System.out.println("Hi");
+		
+	}
+	public boolean isHariSleeping() {
+		return true;
+	}
 }
